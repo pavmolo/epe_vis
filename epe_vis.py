8d56ef461d5b76a0d03a7d8a082faa542746340c
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-01 14:13:31 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -48,17 +48,17 @@ if len(rows_collection) > 0:
     st.bar_chart(data=data, x="SKU", y="Дневной спрос")
 
 
-export_as_pdf = st.button("Export Report")
-def create_download_link(val, filename):
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+    export_as_pdf = st.button("Export Report")
+    def create_download_link(val, filename):
+        b64 = base64.b64encode(val)  # val looks like b'...'
+        return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
-if export_as_pdf:
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(40, 10, report_text)
-    
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+    if export_as_pdf:
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 16)
+        pdf.cell(40, 10, row_container)
 
-    st.markdown(html, unsafe_allow_html=True)
+        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+        st.markdown(html, unsafe_allow_html=True)
