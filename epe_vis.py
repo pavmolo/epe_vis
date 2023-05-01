c52f6eb51f063a8c7b862ce66751cda53189609f
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-01 14:07:00 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -50,8 +50,8 @@ if len(rows_collection) > 0:
 
 export_as_pdf = st.button("Export Report")
 def create_download_link(val, filename):
-b64 = base64.b64encode(val)  # val looks like b'...'
-return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
 if export_as_pdf:
     pdf = FPDF()
