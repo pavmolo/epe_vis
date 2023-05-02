aeea518c43f7d79d6525414edd8d2081d073b41e
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-02 13:10:24 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -75,6 +75,7 @@ if len(rows_collection) > 0:
         finish_time.append(a)
     time_data = pd.concat([pd.Series(stage), pd.Series(time),pd.Series([pd.Timedelta(i, "m") for i in start_time]), pd.Series([pd.Timedelta(i, "m") for i in finish_time])],axis=1)
     time_data.columns = ['Task', 'Время, мин.', 'Start', 'Finish']
+    today = pd.Timestamp('today').strftime('%Y-%m-%d')
     time_data['Start'] = pd.to_datetime(today + ' '  + time_data['Start'].astype(str))
     time_data['Finish'] = pd.to_datetime(today + ' '  + time_data['Finish'].astype(str))
     
