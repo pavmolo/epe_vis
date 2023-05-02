a40d40f8229bcc7257d2486397b5ea73cb581a5b
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-02 07:45:30 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -52,8 +52,7 @@ if len(rows_collection) > 0:
     co_time_in_epe = data['Время переналадки'].sum()
     epe = co_time_in_epe / co_in_a_day
     
-    timeline_data = ((data['Дневной спрос'] * epe).astype('int')) * data['Время цикла']
-    timeline_data = pd.DataFrame(timeline_data, columns = ['Время на производство'], index = data['SKU'])
+    timeline_data = pd.DataFrame(((data['Дневной спрос'] * epe).astype('int')) * data['Время цикла'], columns = ['Время на производство'], index = data['SKU'])
     st.dataframe(data=timeline_data, use_container_width=True)
     
     st.title(f"Времени остается на переналадки в день: {co_in_a_day} минут")
