4ce2fce0f425e8035e39556df06cc14afff53452
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-03 12:14:55 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -95,14 +95,17 @@ if len(rows_collection) > 0:
 
         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-        st.title(f"Времени остается на переналадки в день: {co_in_a_day} минут")
-        st.title(f"Времени переналадки в цикле EPE: {co_time_in_epe} минут")
-        st.title(f"EPE = {epe} дней")
-        st.title(f"EPE = {epe * work_minutes} минут")
+        #st.title(f"Времени остается на переналадки в день: {co_in_a_day} минут")
+        #st.title(f"Времени переналадки в цикле EPE: {co_time_in_epe} минут")
+        #st.title(f"EPE = {epe} дней")
+        #st.title(f"EPE = {epe * work_minutes} минут")
         
         col1, col2 = st.columns(2)
         col1.metric("Времени остается на переналадки в день", f"{co_in_a_day} минут")
         col2.metric("Времени переналадки в цикле EPE", f"{co_time_in_epe} минут")
+        col3, col4 = st.columns(2)
+        col3.metric("EPE в днях", f"{epe} дней")
+        col4.metric("EPE в минутах", f"{epe * work_minutes} минут")
         
     else:
         st.title(f"В цикле не остается времени на переналадку. EPE отрицательна и составляет {epe} дней")
