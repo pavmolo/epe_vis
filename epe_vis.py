179ff917a02547c70d387ff0793aa5b31e97ad42
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-04 15:52:36 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -37,7 +37,7 @@ def generate_row(row_id):
     row_columns[4].button("🗑️", key=f"del_{row_id}", on_click=remove_row, args=[row_id])
     return {"name": row_name, "qty": row_qty, "cycle": row_cycle, "co": row_co}
 
-if uploaded_file:
+if not uploaded_file:
     menu = st.columns(2)
     with menu[0]:
         st.button("Добавить SKU", on_click=add_row)
