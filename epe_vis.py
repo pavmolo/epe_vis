00ef48611915dbace009a0bd894519b1e9ca5204
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-04 15:17:21 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pandas as pd
 import uuid
@@ -23,85 +24,46 @@ def generate_row(row_id):
     row_qty = row_columns[1].number_input("Дневной спрос", step=1, key=f"nbr_{row_id}")
     row_cycle = row_columns[2].number_input("Время цикла", step=2, key=f"time_{row_id}")
     row_co = row_columns[3].number_input("Время переналадки", step=3, key=f"co_{row_id}")
-    row_columns[4].button("🗑️", key=f"del_{row_id}", on_click=remove_row, args=[row_id])
-    return {"name": row_name, "qty": row_qty, "cycle": row_cycle, "co": row_co}
-st.title("Данные по SKU на оборудовании")
-for row in st.session_state["rows"]:
-    row_data = generate_row(row)
-    rows_collection.append(row_data)
-menu = st.columns(2)
-with menu[0]:
-    st.button("Добавить SKU", on_click=add_row)
+     row_columns[4].button("🗑️", key=f"del_{row_id}", on_click=remove_row, args=[row_id])
+     return {"name": row_name, "qty": row_qty, "cycle": row_cycle, "co": row_co}
+
+ st.title("Данные по SKU на оборудовании")
+
+ for row in st.session_state["rows"]:
+     row_data = generate_row(row)
+     rows_collection.append(row_data)
+
+ menu = st.columns(2)
+
+ with menu[0]:
+     st.button("Добавить SKU", on_click=add_row)
+Expand Down
+Expand Up	@@ -99,6 +93,13 @@ def generate_row(row_id):
+         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+ if len(rows_collection) > 0:
+     st.title("Данные по SKU на оборудовании")
+
+     for row in st.session_state["rows"]:
+         row_data = generate_row(row)
+         rows_collection.append(row_data)
+
+     menu = st.columns(2)
+     st.subheader("Показатели")
+     data = pd.DataFrame(rows_collection)
+     data.rename(columns={"name": "SKU", "qty": "Дневной спрос", "cycle": "Время цикла", "co": "Время переналадки"}, inplace=True)
+    co_in_a_day = work_minutes - (data['Дневной спрос'] * data['Время цикла']).sum()
+    co_time_in_epe = data['Время переналадки'].sum()
+    epe = co_time_in_epe / co_in_a_day
+    if epe > 0:
     
-if len(rows_collection) == 0:
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        data = pd.read_excel(uploaded_file)
-        st.subheader("Показатели")
-        co_in_a_day = work_minutes - (data['Дневной спрос'] * data['Время цикла']).sum()
-        co_time_in_epe = data['Время переналадки'].sum()
-        epe = co_time_in_epe / co_in_a_day
         t_data = ((data['Дневной спрос'] * epe).astype('int')) * data['Время цикла']
         timeline_data = pd.concat([data['SKU'], t_data, data['Время переналадки']],axis=1)
         timeline_data.columns = ['SKU', 'Время производства', 'Время переналадки']
+    
         stage = []
         time = []
         for sku in data['SKU']:
-            stage.append(f'Производство {sku}')
-            stage.append(f'Переналадка')
-            time.append(timeline_data[timeline_data['SKU'] == sku]['Время производства'].iloc[0])
-            time.append(timeline_data[timeline_data['SKU'] == sku]['Время переналадки'].iloc[0])
-        finish_time = []
-        start_time = []
-        a = 0
-        for i in time:
-            start_time.append(a)
-            a = a + i
-            finish_time.append(a)
-        #today = pd.Timestamp('today').strftime('%Y-%m-%d')
-        #now = datetime.datetime.now()
-        #time_data = pd.concat([pd.Series(stage), pd.Series(time), (now + pd.Series([pd.Timedelta(minutes=i) for i in start_time])), (now + pd.Series([pd.Timedelta(minutes=i) for i in finish_time]))],axis=1)
-        #time_data['Start'] = pd.to_datetime(time_data['Start'])
-        #time_data['Finish'] = pd.to_datetime(time_data['Finish'])
-        time_data = pd.concat([pd.Series(stage), pd.Series(time), pd.Series(start_time), pd.Series(finish_time)],axis=1)
-        time_data.columns = ['Task', 'Description', 'Start', 'Finish']
-        #st.dataframe(data=time_data, use_container_width=True)
-        #fig = px.timeline(time_data, x_start="Start", x_end="Finish", y="Task", Description = 'Время, мин.')
-        fig = ff.create_gantt(time_data, bar_width = 0.4, index_col='Task')
-        fig.update_layout(xaxis_type='linear', autosize=False)
-        fig.layout.update({'title': 'Схема цикла EPE'})
-        #fig.update_layout(hovermode="Description")
-        fig.update_layout(xaxis_title="Линия времени в минутах", yaxis_title="Операция")
-        fig.show()
-        fig.update_yaxes(autorange="reversed")
-        col1, col2 = st.columns(2)
-        col1.metric("Времени остается на переналадки в день", f"{co_in_a_day} минут")
-        col2.metric("Времени переналадки в цикле EPE", f"{co_time_in_epe} минут")
-        col3, col4 = st.columns(2)
-        col3.metric("EPE в днях", f"{epe} дней")
-        col4.metric("EPE в минутах", f"{epe * work_minutes} минут")
-        st.subheader("График EPE")
-        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-if len(rows_collection) > 0:
-    st.subheader("Показатели")
-    data = pd.DataFrame(rows_collection)
-    data.rename(columns={"name": "SKU", "qty": "Дневной спрос", "cycle": "Время цикла", "co": "Время переналадки"}, inplace=True)
-     co_in_a_day = work_minutes - (data['Дневной спрос'] * data['Время цикла']).sum()
-     co_time_in_epe = data['Время переналадки'].sum()
-     epe = co_time_in_epe / co_in_a_day
-     if epe > 0:
-
-         t_data = ((data['Дневной спрос'] * epe).astype('int')) * data['Время цикла']
-         timeline_data = pd.concat([data['SKU'], t_data, data['Время переналадки']],axis=1)
-         timeline_data.columns = ['SKU', 'Время производства', 'Время переналадки']
-
-     t_data = ((data['Дневной спрос'] * epe).astype('int')) * data['Время цикла']
-     timeline_data = pd.concat([data['SKU'], t_data, data['Время переналадки']],axis=1)
-     timeline_data.columns = ['SKU', 'Время производства', 'Время переналадки']
-     if epe > 0:
-         stage = []
-         time = []
-         for sku in data['SKU']:
             stage.append(f'Производство {sku}')
             stage.append(f'Переналадка')
             time.append(timeline_data[timeline_data['SKU'] == sku]['Время производства'].iloc[0])
