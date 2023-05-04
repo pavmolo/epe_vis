a945bc2272168716b4fbef30f1df2df8eceb2a2f
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-05-04 12:14:46 +0300

    Update epe_vis.py

--- epe_vis.py
+++ epe_vis.py
@@ -105,12 +105,12 @@ if len(rows_collection) > 0:
     co_in_a_day = work_minutes - (data['Дневной спрос'] * data['Время цикла']).sum()
     co_time_in_epe = data['Время переналадки'].sum()
     epe = co_time_in_epe / co_in_a_day
-    
-    
-    t_data = ((data['Дневной спрос'] * epe).astype('int')) * data['Время цикла']
-    timeline_data = pd.concat([data['SKU'], t_data, data['Время переналадки']],axis=1)
-    timeline_data.columns = ['SKU', 'Время производства', 'Время переналадки']
     if epe > 0:
+    
+        t_data = ((data['Дневной спрос'] * epe).astype('int')) * data['Время цикла']
+        timeline_data = pd.concat([data['SKU'], t_data, data['Время переналадки']],axis=1)
+        timeline_data.columns = ['SKU', 'Время производства', 'Время переналадки']
+    
         stage = []
         time = []
         for sku in data['SKU']:
